65fb64cad3900fab843f853431f9510a20a85355
vogelch <christoph.vogel@uzh.ch>
2018-11-27 17:47:26 +0100

    trying to resolve issue with DLL not found

--- src/client/AM.PA.MonitoringTool/AudioTracker/Daemon.cs
+++ src/client/AM.PA.MonitoringTool/AudioTracker/Daemon.cs
@@ -873,6 +873,8 @@ namespace AudioTracker
 
         private static void ConvertWavToMp3(string WavFile, string Mp3FileName)
         {
+            CheckAddBinPath();
+
             using (var resultMemoryStream = new MemoryStream())
             using (var reader = new WaveFileReader(WavFile))
             using (var writer = new LameMP3FileWriter(Mp3FileName, reader.WaveFormat, 16/*LAMEPreset.VBR_90*/))
@@ -900,6 +902,21 @@ namespace AudioTracker
             ...
         }
         */
+
+        public static void CheckAddBinPath()
+        {
+            // find path to 'bin' folder
+            var binPath = Path.Combine(new string[] { AppDomain.CurrentDomain.BaseDirectory, "bin" });
+            // get current search path from environment
+            var path = Environment.GetEnvironmentVariable("PATH") ?? "";
+
+            // add 'bin' folder to search path if not already present
+            if (!path.Split(Path.PathSeparator).Contains(binPath, StringComparer.CurrentCultureIgnoreCase))
+            {
+                path = string.Join(Path.PathSeparator.ToString(), new string[] { path, binPath });
+                Environment.SetEnvironmentVariable("PATH", path);
+            }
+        }
 
         public bool ByteArrayToFile(string byteArrayTargetFileName, byte[] inputByteArray)
         {
